a4502d0a670711a4b5fe3f07b9114e94857a4fbc
Mario Kaack <mario@shortrope.com>
2016-07-03 17:50:29 -0700

    Create Menu
    Style 3dot hamburger
    Slide menu in and out from right
    Hide and reveal pages

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,14 @@ document.addEventListener('DOMContentLoaded', function () {
       nextBtn = document.getElementById('next_btn'),
       suggestionInput = document.getElementById('suggestion_input'),
       addBtn = document.getElementById('add_btn'),
-      listUL = document.getElementById('list');
+      listUL = document.getElementById('list'),
+      menu = document.getElementById('menu'),
+      menuToggle = document.getElementById('menu_toggle'),
+      body = document.getElementsByTagName('body')[0],
+      editMenuItem = document.getElementById('edit_menu_item'),
+      doneMenuItem = document.getElementById('done_menu_item'),
+      appPage = document.getElementById('app'),
+      managePage = document.getElementById('manage');
   
   
   ////////////////////////////////////////////////////////////////////////
@@ -66,6 +73,29 @@ document.addEventListener('DOMContentLoaded', function () {
   
   ////////////////////////////////////////////////////////////////////////
   // Event Listeners
+  
+  menu.addEventListener('click', function toggleIn(evt) {
+    evt.stopPropagation();
+    menuToggle.classList.toggle('slide-in'); 
+    
+    body.addEventListener('click', function toggleOut(outEvt) {
+      menuToggle.classList.toggle('slide-in'); 
+      body.removeEventListener('click', toggleOut);
+    });
+    
+    editMenuItem.addEventListener('click', function displayEditList(displayEvt) {
+      appPage.classList.add('hidden');
+      managePage.classList.remove('hidden');
+      editMenuItem.classList.add('hidden');
+      doneMenuItem.classList.remove('hidden');
+    });
+    doneMenuItem.addEventListener('click', function displayDoneList(displayEvt) {
+      appPage.classList.remove('hidden');
+      managePage.classList.add('hidden');
+      doneMenuItem.classList.add('hidden');
+      editMenuItem.classList.remove('hidden');
+    });
+  });
   
   nextBtn.addEventListener('click', function nextFn() {
     displaySuggestion(nextSuggestion());
